8ed80e4a9f7ada73627257a27338dacf2247c25a
KacperGil <kacpergil1610@gmail.com>
2022-11-22 20:31:11 +0100

    dodanie zapytania o imie i wiek

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Kacper";
-        int age=18;
-        System.out.println("Cześć mam na imie "+name+" i mam "+age+" lat");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String name=scanner.nextLine();
+        System.out.println("Ile masz lat?");
+        int age=scanner.nextInt();
+
+        System.out.println("Cześć "+name+" masz "+age+" lat/a");
 
     }
 }
